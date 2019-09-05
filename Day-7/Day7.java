e3df44ab26f16775a2c80ea9af8491ec695733ba
Ankit Mansinghani <ankitmansinghani21@gmail.com>
2019-09-05 13:47:44 +0530

    Updated

--- Day-7/Day7.java
+++ Day-7/Day7.java
@@ -1,51 +1,48 @@
-import java.util.Arrays;
+package question;
+
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.*;
+import java.util.Set;
+import java.util.Stack;
 
-/**
- * Day7
- */
 public class Day7 {
+	static public Set<Stack<Integer>> opSet = new HashSet<>();
 
-    static public List<Stack<Integer>> opSet = new ArrayList<>();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    	Stack<Integer> stack = new Stack<>();
+	    	getCountUtil(stack,12345);
+	        System.out.println("length of set : "+opSet.size());		
 
-    public static void main(String[] args) {
-        getCount(12345);
-    }
-
-    public static void getCount(int x) {
-        Stack<Integer> stack = new Stack<>();
-        getCountUtil(stack,x);
-    }
-
-    public static void getCountUtil(Stack<Integer> stack,int x) {
-        if(x>0)
-        {
-            System.out.println("Every iteration x value : "+x);
-            if(x%100<25)
-            {
-                stack.push(x%10);
-                getCountUtil(stack, x/10);
-                stack.pop();
-                stack.push(x%100);
-                getCountUtil(stack, x/100);
-                stack.pop();
-            }
-            else
-            {
-                stack.push(x%10);
-                getCountUtil(stack, x/10);
-                stack.pop();
-            }
-        }
-        else
-        {
-            System.out.println("***********Combination****************");
-            System.out.println(Arrays.toString(stack.toArray()));
-            opSet.add(stack);
-            System.out.println("length of set : "+opSet.size());
-        }
-        
-    }
-}
+	}
+	    public static void getCountUtil(Stack<Integer> stack,int x) {
+	        if(x!=0)
+	        {
+	            //System.out.println("Every iteration x value : "+x);
+	            stack.push(x%10);
+	            getCountUtil(stack, x/10);
+	            stack.pop();
+	            
+	            if(x%100<25 && x>25 && x%100!=0)
+	            {
+	            	stack.push(x%100);
+	                getCountUtil(stack, x/100);
+	                stack.pop();
+	            }
+	            else if(x<25)
+	            {                
+	                stack.push(x);
+	                getCountUtil(stack, 0);
+	                stack.pop();
+	            }
+	            
+	        }
+	        else
+	        {
+	            System.out.println("******Valid Combination****************");
+	            System.out.println(stack.toString());
+	            opSet.add(stack);
+	        
+	        }
+	        
+	    }
+	}
